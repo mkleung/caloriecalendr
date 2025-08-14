7835ad070b4903746463bf6e96de791ac07b2124
Mike <mpakleung@gmail.com>
2025-08-14 15:01:42 -0400

    Build the structure of the app

---
+++ app/dashboard/page.js
@@ -0,0 +1,29 @@
+// import { Main } from "next/document";
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+  title: "CalorieCalendr - Dashboard",
+};
+
+
+export default function DashboardPage() {
+
+    const isAuthenticated = true
+    let children = (
+        <Login />
+    )
+
+    if (isAuthenticated){
+        children = (
+            <Dashboard />
+        )
+    }
+
+    return (
+        <Main>
+            {children}
+        </Main>
+    )
+}
